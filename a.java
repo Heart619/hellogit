51868a911274d9f730db9c2b518dfb0f206d866c
Heart619 <2508615597@qq.com>
2022-07-26 09:48:28 +0800

    cs

--- a.java
+++ a.java
@@ -2,6 +2,11 @@ public class a {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		say("hello");
+	}
+
+	public String say(String msg) {
+		System.out.println(msg);
 	}
 }
 
